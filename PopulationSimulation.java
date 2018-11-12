50ec101a8b48998cb10285b3be95db0e02e37a0a
Pankaj Kumar <805.bluebell@gmail.com>
2018-11-12 14:09:13 +0000

    Now human can take birth but still relatives are not defined

--- PopulationSimulation.java
+++ PopulationSimulation.java
@@ -1,17 +1,55 @@
 //First java program
+import java.util.Random;
 
-public class PopulationSimulation {
+public class PopulationSimulation
+{
+    public long count = 0;
 
-    public static class Human {
+    //Basic Structure of a human
+    public static class Human
+    {
         long pan;
-        char sex;
+        boolean sex; //True is Male
         Human father;
         Human mother;
         Human nextSibling;
-
+        Human spouse;
+        Human()
+        {
+            this.father = null;
+            this.mother = null;
+            this.nextSibling = null;
+            this.spouse = null;
+        }
     }
 
-    public static void main(string[] args) {
+    //Taking a birth
+    public Human birth()
+    {
+        Human box = new Human();
+        box.pan = count + 1;
+        box.sex = maleOrFemale();
+        return box;
+    }
+
+    //Detemining sex
+    public boolean maleOrFemale ()
+    {
+        Random rand = new Random();
+        int randNumber = rand.nextInt(1000);
+        if(randNumber % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
 
     }
 }
