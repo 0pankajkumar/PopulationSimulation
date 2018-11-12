703d499372976c82d168a5e0ec49a7e7d0b88446
Pankaj Kumar <805.bluebell@gmail.com>
2018-11-12 13:26:55 +0000

    Human structure defined

--- PopulationSimulation.java
+++ PopulationSimulation.java
@@ -1,1 +1,17 @@
 //First java program
+
+public class PopulationSimulation {
+
+    public static class Human {
+        long pan;
+        char sex;
+        Human father;
+        Human mother;
+        Human nextSibling;
+
+    }
+
+    public static void main(string[] args) {
+
+    }
+}
