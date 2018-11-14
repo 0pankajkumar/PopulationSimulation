1f5a3ba80a695bd06631353d36913ee4143b0f6a
Pankaj Kumar <805.bluebell@gmail.com>
2018-11-14 07:52:14 +0000

    Functions are decided but multi-threading is still not understood how to implement. It always shows thread 1 running

--- PopulationSimulation.java
+++ PopulationSimulation.java
@@ -6,7 +6,7 @@ public class PopulationSimulation
     public static long count = 0;
 
     //Basic Structure of a human
-    public static class Human
+    public static class Human extends Thread
     {
         long pan;
         boolean sex; //True is Male & False is Female
@@ -23,15 +23,23 @@ public class PopulationSimulation
             this.sex = maleOrFemale();
             this.pan = ++count;
         }
-    }
+        public void run()
+        {
+            try
+            {
+                // Displaying the thread that is running
+                System.out.println ("Thread " +
+                      Thread.currentThread().getId() +
+                      " is running");
+                try{Thread.sleep(100);}catch(InterruptedException e){System.out.println(e);}
 
-    //Taking a birth
-    public Human birth()
-    {
-        Human box = new Human();
-        box.pan = count + 1;
-        box.sex = maleOrFemale();
-        return box;
+            }
+            catch (Exception e)
+            {
+                // Throwing an exception
+                System.out.println ("Exception is caught");
+            }
+        }
     }
 
     //Detemining sex
@@ -49,23 +57,42 @@ public class PopulationSimulation
         }
     }
 
+    //Marriage
+    public static void marriage()
+    {
+
+    }
+
+    //Mating
+    public static void mate()
+    {
+
+    }
+
+    //live
+    public static void live()
+    {
+
+    }
 
     public static void main(String[] args)
     {
         Human first = new Human();
         Human second = new Human();
         Human third = new Human();
-        System.out.println("first is " + first.sex);
-        System.out.println("with pan " + first.pan);
-        System.out.println();
+        first.run();
+        try
+        {
+            first.wait();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception is caught while waiting");
+        }
+        second.run();
+        third.run();
 
-        System.out.println("second is " + second.sex);
-        System.out.println("with pan " + second.pan);
-        System.out.println();
 
-        System.out.println("third is " + third.sex);
-        System.out.println("with pan " + third.pan);
-        System.out.println();
 
         for (int i = 0; i < 10; i++)
         {
