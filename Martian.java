310da733c3c97fbcccd50872f8f60d1fc6b5d18e
Pankaj Kumar <805.bluebell@gmail.com>
2018-11-28 14:44:20 +0000

    version3.1: Three generations at a time create the world. Mating yet to be implemented

--- Martian.java
+++ Martian.java
@@ -4,54 +4,68 @@ import java.util.*;
 
 public class Martian
 {
+    public static ArrayList<ArrayList> generationList = new ArrayList<ArrayList>(); //List of a generations
+
+    public static ArrayList<boolean[]> familyList = new ArrayList<boolean[]>(); //List of families belonging to a generation
+
     public void generation()
     {
-        ArrayList<boolean[]> arrli = new ArrayList<boolean[]>(); //List of a generation
-
-        for loop till 2000 assuming 100 years for each generation
+        for (int u = 0; u < 2000; u++) //loop till 2000 assuming 100 years for each generation
         {
 
-            generation = mate(gen1); //Includes die. Return a ArrayList of newly created generation
-                                        //Gen1 is passed, Gen2 will be returned
-            genLi.add(generation); //New generation is added to List of generations
+            mate(); //Includes die.
 
 
 
-
-
-
-
-
-
-
-
-
-            int offsprings = getOffsprings(); //Calculate No. of offsprings of first mating
-            //Array of all siblings. True is male & False female
-            for loop till a
         }
     }
 
     //Mate & die
     public void mate()
     {
-        //Mate
+        /*
+        -------------------------------------------------------------------------
+        Mate
+        -------------------------------------------------------------------------
+        # Three generations will be present at a time. Gen1 is old & had already mated.
+            Only a few must be left to mate in Gen1.
+        # Gen2 is the one which will mate the most creating a Gen3.
+            Gen3 will just get populated for time being
+        # Before creation of Gen4 all references to Gen1 is removed for garbage collector
+            and they are left to die.
+        -------------------------------------------------------------------------
+        */
+        int randomFamilies[] = RandomizeArray(familyList.size());
+        for (int i = 0; i < generationList.get(1).size(); i++)
+        {
+            generationList.get(i);
+        }
+
         //null or kill those two individuals
+
+
+        //generationList.add(); //New generation is added to List of generations
+
     }
 
-    //Detemining sex randomly
-    public boolean maleOrFemale ()
+    //Just makes a list of random numbers to randomize families while mating
+    public static int[] RandomizeArray(int a)
     {
-        Random rand = new Random();
-        int randNumber = rand.nextInt(1000);
-        if(randNumber % 2 == 0)
+        int array[] = new int[a];
+        for (int i = 0; i < a; i++)
         {
-            return true;
+            array[i] = i;
         }
-        else
+        Random rgen = new Random(); // Random number generator
+        for (int i = 0; i < array.length; i++)
         {
-            return false;
+            int randomPosition = rgen.nextInt(array.length);
+            int temp = array[i];
+            array[i] = array[randomPosition];
+            array[randomPosition] = temp;
         }
+        return array;
     }
+
 
 }
