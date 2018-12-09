f5b64f0053817d99f6125fd2b803f98be4dba71c
Pankaj Kumar <805.bluebell@gmail.com>
2018-12-09 16:34:09 +0000

    Coded with new logic. Now check flow of control. It goes into infinite loop for no reason. Then implement 2000 years of generations building. Then store generations data to show.

--- Jupiterite.java
+++ Jupiterite.java
@@ -1,37 +1,87 @@
 //Pseudo code only
-package Helper;
+
 
 import java.util.*;
 
 public class Jupiterite
 {
-    public static ArrayList<ArrayList> generations = new ArrayList<ArrayList>(); //List of a generations
+    public static ArrayList<ArrayList<Integer>> generations = new ArrayList<ArrayList<Integer>>(); //List of a generations
 
 
     //Mate & die
-    public void mate()
+    public static void wave()
     {
-        Go to second generation
-            While loop : Till list of available Global families in Generation two is >= 2
-                While loop : Till list of available families is >= 2
-                    Maintain a local copy of available familes
-                    Select any family from list of available families
-                    If its count is already zero
-                        Remove the family number from local list of available families
-                        repeat the loop
-                    Else get out of While loop
+        ArrayList<Integer> zeroGen = generations.get(0);
+        ArrayList<Integer> firstGen = generations.get(1);
+        ArrayList<Integer> secondGen = generations.get(2);
+        Random rand = new Random();
+        int noOfFamiles = firstGen.size();
+        int localNoOfFamiles = noOfFamiles;
 
-                While loop
-                    Maintain a local copy of available familes
-                    Select any other family but not neighbouring family or the smae family as selected from above loop
-                    If they can mate
-                        Add a New family in generation three
-                        Decrease Global count in both families
-                    Else
-                        Remove the family number from local list of available families
-                        Repeat the While loop
-        first generation = second generation
-        second generation = third generation
-        third generation = Null
+            while(noOfFamiles >= 2) //Ensures that almost all familes of generation participate in mating season
+            {
+                int selection = rand.nextInt(localNoOfFamiles);
 
-    >>Mate will be reperated 2000 times For 2 thousand generations
+                System.out.println("Inside Loop 1");
+                //This will not allow 0 family size to enter competition
+                while(firstGen.get(selection) == 0)
+                {
+                    System.out.println("Inside Loop 2");
+                    firstGen.remove(selection);
+                    selection = rand.nextInt(firstGen.size());
+                }
+
+
+                int selection2 = rand.nextInt(firstGen.size());
+
+                while(selection2 == 0 || selection2 == selection || selection2 == selection+1 || selection2 == selection-1)
+                {
+                    System.out.println("Inside Loop 3");
+                    selection2 = rand.nextInt(firstGen.size());
+                }
+
+                if((rand.nextInt() % 2) == 1) //Just making it look random. Combined sex determination & male-female matching condition for marriage
+                {
+                    //Adding children to the couple in next generation
+                    secondGen.add(rand.nextInt(10));
+                }
+            }
+
+        System.out.println("Outside all Loops");
+
+        zeroGen = firstGen;
+        firstGen = secondGen;
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        secondGen = temp;
+    }
+
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        for(int i = 0; i < 20; i++)
+        {
+            temp.add(rand.nextInt(10));
+        }
+        generations.add(temp);
+
+        temp.clear();
+        for(int i = 0; i < 30; i++)
+        {
+            temp.add(rand.nextInt(10));
+        }
+        generations.add(temp);
+
+        temp.clear();
+        for(int i = 0; i < 30; i++)
+        {
+            temp.add(rand.nextInt(10));
+        }
+        generations.add(temp);
+
+
+
+        wave(); //Run thousand times
+    }
+}
---
+++ Test3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Test3
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = a;
+        a = 20;
+        System.out.println(b);
+    }
+}
