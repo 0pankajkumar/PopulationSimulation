5676b7dfc07d4ef8c1e2b2502cf3854a08c006ce
Pankaj Kumar <805.bluebell@gmail.com>
2018-12-16 07:17:20 +0000

    No swapping happening. All previous bugs removed. Yeh!

--- Jupiterite.java
+++ Jupiterite.java
@@ -18,7 +18,7 @@ public class Jupiterite
         int noOfFamiles = firstGen.size();
         int localNoOfFamiles = noOfFamiles;
 
-            while(firstGen.size() >= 2) //Ensures that almost all familes of generation participate in mating season
+            while(firstGen.size() >= 4) //Ensures that almost all familes of generation participate in mating season
             {
                 int selection = rand.nextInt(firstGen.size());
 
@@ -26,7 +26,7 @@ public class Jupiterite
                 //This will not allow 0 family size to enter competition
                 while(firstGen.get(selection) == 0 && firstGen.size() >= 2)
                 {
-                    System.out.println("Inside Loop 2");
+                    //System.out.println("Inside Loop 2");
                     firstGen.remove(selection);
                     selection = rand.nextInt(firstGen.size());
                     System.out.println(generations);
@@ -36,12 +36,11 @@ public class Jupiterite
                 int selection2 = rand.nextInt(firstGen.size());
 
                 boolean flag = false;
-                while(flag == false)
-                {
+
                     //Trap for invalid matches
                     while(firstGen.get(selection2) == 0 || selection2 == selection || selection2 == selection+1 || selection2 == selection-1)
                     {
-                        System.out.println("Inside Loop 3");
+                        //System.out.println("Inside Loop 3");
                         if(firstGen.get(selection2) == 0)
                         {
                             firstGen.remove(selection2);
@@ -49,8 +48,6 @@ public class Jupiterite
                         selection2 = rand.nextInt(firstGen.size());
                     }
 
-                    if((rand.nextInt() % 2) == 1) //Just making it look random. Combined sex determination & male-female matching condition for marriage
-                    {
                         //Adding children to the couple in next generation
                         secondGen.add(rand.nextInt(10));
 
@@ -59,38 +56,29 @@ public class Jupiterite
                         firstGen.set(selection,foo);
                         firstGen.set(selection2,boo);
 
-                        /*Deleteing family size in firstGen pairs
-                        if(firstGen.get(selection) <= 0)
-                        {
-                            firstGen.remove(selection);
-                        }
-                        if(firstGen.get(selection2) <= 0)
-                        {
-                            firstGen.remove(selection2);
-                        }*/
 
                         flag = true;
-                    }
-                    else
+
+                //Removing all zero elements
+                for(int i = 0; i < firstGen.size(); i++)
+                {
+                    if(firstGen.get(i) == 0)
                     {
-                        selection2 = rand.nextInt(firstGen.size());
+                        firstGen.remove(i);
                     }
                 }
 
-                    /*noOfFamiles = firstGen.size();
-                    System.out.println(generations);
-                    System.out.println("\n");
 
-                    Scanner input = new Scanner(System.in);
-                    String myString = input.next();*/
             }
 
         System.out.println("Outside all Loops");
+        System.out.println(generations);
 
         zeroGen = firstGen;
         firstGen = secondGen;
         ArrayList<Integer> temp = new ArrayList<Integer>();
         secondGen = temp;
+        System.out.println(generations.get(1));
     }
 
     public static void initialStuff()
@@ -160,9 +148,9 @@ public class Jupiterite
     {
         initialStuff();
         wave(); //Run thousand times
-
-        System.out.println(generations.get(0).size());
-        System.out.println(generations.get(1).size());
-        System.out.println(generations.get(2).size());
+        wave();
+        System.out.println("Gen 1 size" + generations.get(0).size());
+        System.out.println("Gen 2 size" + generations.get(1).size());
+        System.out.println("Gen 3 size" + generations.get(2).size());
     }
 }
